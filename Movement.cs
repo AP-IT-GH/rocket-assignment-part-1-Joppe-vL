9431c375671387242154862557a7923a974be8e5
Joppe-vL <91462808+Joppe-vL@users.noreply.github.com>
2022-10-28 19:08:34 +0200

    Add files via upload

---
+++ Movement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float thrusterForce = 10f;
+    [SerializeField] float tiltingForce = 10f;
+
+    Rigidbody rocket;
+
+
+
+    private void Awake()
+    {
+        rocket = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float tilt = Input.GetAxis("Horizontal");
+        if (!Mathf.Approximately(tilt, 0f))
+        {
+            rocket.freezeRotation = true;
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + (new Vector3(0f, 0f, (tilt * tiltingForce * Time.deltaTime))));
+        }
+
+        rocket.freezeRotation = false;
+    }
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rocket.AddRelativeForce(Vector3.up * thrusterForce * Time.deltaTime);
+        }
+    }
+}
